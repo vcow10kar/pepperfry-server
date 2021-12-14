61eabde7e3325327f171fd20920aa4def11fa41e
vcow10kar <vkawthankar@gmail.com>
2021-12-14 13:35:23 +0530

    cart and cartDetails controllers added to index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,20 @@ const cors = require("cors");
 
 const brandController = require('./controllers/brand.controller');
 const categoryController = require('./controllers/category.controller');
+const cartController = require('./controllers/cart.controller');
+const cartDetailsController = require('./controllers/cartDetails.controller');
 
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+
+
 app.use('/brands', brandController)
 app.use('/category',categoryController)
+app.use('/cart', cartController);
+app.use('/cartDetails', cartDetailsController);
 
 
 module.exports = app;
