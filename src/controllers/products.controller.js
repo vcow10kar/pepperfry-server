499f23cb2b492b47e9f7e74ba075beac8bf90243
madhuvenkat <madhuvenkatmedicharla@gmail.com>
2021-12-14 14:54:16 +0530

    products schema and controllers

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -4,23 +4,41 @@ const router = express.Router();
 
 const Products = require("../models/products.model")
 
-router.get("/",async(req,res)=>{
-    return res.status(201).send("get--->proucts")
+router.get("/", async (req, res) => {
+    
+    const products = await Products.find().lean().exec()
+    
+    return res.status(201).send(products)
 })
 
-router.post("/",async(req,res)=>{
+
+router.get("/:id", async (req, res) => {
+    
+    const product = await Products.findById(req.params.id)
+    
+    return res.status(201).send(product)
+})
+
+
+router.post("/", async (req, res) => {
 
     const product = await Products.create(req.body);
 
     return res.status(200).send(product)
 })
 
-router.patch("/",async(req,res)=>{
-    return res.status(201).send("patch--->product")
+router.patch("/:id", async (req, res) => {
+
+    const product = await Products.findByIdAndUpdate(req.params.id, req.body, { new: true })
+
+    return res.status(201).send(product)
 })
 
-router.delete("/",async(req,res)=>{
-    return res.status(201).send("Delete--->product")
+router.delete("/:id", async (req, res) => {
+
+    const product = await Products.findByIdAndDelete(req.params.id);
+
+    return res.status(201).send(product)
 })
 
 module.exports = router
